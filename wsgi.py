17c5f1dea5c8159732cbb9f51d5165d8d8792f5e
Krishanu19 <48989056+Krishanu19@users.noreply.github.com>
2019-12-18 00:37:58 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,10 +7,10 @@ application = Flask(__name__)
 @application.route("/")
 #@application.route('/', methods=['GET', 'POST'])
 def hello():
-    return "<h1>Hello World! From fertile Mind!</h1>"
-    #return render_template('https://github.com/Krishanu19/os-sample-python.git/Template/index.html')
+    #return "<h1>Hello World! From fertile Mind!</h1>"
+    return render_template('index.html')
 
-@application.route("/ab/")
+@application.route("/db/")
 def dbconnect():
     #mydb = mysql.connector.connect(
     #     host="mysql.gamification.svc.cluster.local",
@@ -18,12 +18,12 @@ def dbconnect():
     #     passwd="welcome1",
     #     database=sampledb
     #    )
-    conn=MySQLdb.connect(host='mysql.gamification.svc.cluster.local',user='xxuser',passwd='welcome1')
-    #return "DB Connected"
+    conn=MySQLdb.connect(host='mysql.gamification.svc.cluster.local',user='xxuser',passwd='welcome1')    
     mycursor = conn.cursor()
     sql = "select list_price from XXIBM_PRODUCT_PRICING where item_number=1001"
-    mycursor.execute(sql)
-    data=conn.fetchall()    
+    return "DB Connected"
+    #mycursor.execute(sql)
+    #data=conn.fetchall()    
 
 if __name__ == "__main__":
     application.run(debug=TRUE)
