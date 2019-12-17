cec78dd2ff917ad331f58e9bb6369042a455224d
Krishanu19 <48989056+Krishanu19@users.noreply.github.com>
2019-12-17 23:50:04 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -19,12 +19,11 @@ def dbconnect():
     #     database=sampledb
     #    )
     conn=MySQLdb.connect(host='mysql.gamification.svc.cluster.local',user='xxuser',passwd='welcome1')
-    #return "DB Connected"
+    return "DB Connected"
     mycursor = conn.cursor()
-    sql = "select list_price,'||'discount as 'list_price||discount' from XXIBM_PRODUCT_PRICING where item_number=1001"
+    sql = "select list_price from XXIBM_PRODUCT_PRICING where item_number=1001"
     mycursor.execute(sql)
-    data=conn.fetchall()
-    return data
+    data=conn.fetchall()    
 
 if __name__ == "__main__":
     application.run()
