76a8cba1048ff4b536da49ca332a4a0d5d9428a8
Nandita Saha <nasaha12@in.ibm.com>
2019-12-18 15:16:19 +0530

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -22,12 +22,12 @@ def dbconnect():
     conn=MySQLdb.connect(host='mysql.gamification.svc.cluster.local',user='xxuser',passwd='welcome1')    
     mycursor = conn.cursor()
     mycursor.execute("select list_price from XXIBM_PRODUCT_PRICING where item_number=1001")
-	rv = mycursor.fetchall()
-	return str(rv)
+    rv = mycursor.fetchall()
+    return str(rv)
     #return "DB Connected"
     #mycursor.execute(sql)
     #data=conn.fetchall()    
-	#return render_template('index.html')
-	
+    #return render_template('index.html')
+
 if __name__ == "__main__":
     application.run(debug=TRUE)
