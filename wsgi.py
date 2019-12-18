cbdfe5e8a0ece470dc64100b73601c8d088ef765
Nandita Saha <nasaha12@in.ibm.com>
2019-12-18 18:39:42 +0530

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -2,32 +2,19 @@ from flask import Flask, render_template
 
 application = Flask(__name__, template_folder='templates')
 
-#@application.route('/')
+@application.route('/')
 #def index():
 #    return render_template('index.html')
 
 #@application.route("/db/")
 
-class Database:
-    def __init__(self):
-            host = "mysql.gamification.svc.cluster.loca"
-            user = "xxuser"
-            password = "welcome1"
-            db = "XXIBM_PRODUCT_PRICING"
-            self.con = MySQLdb.connect(host='mysql.gamification.svc.cluster.local',user='xxuser',passwd='welcome1')
-            self.cur = self.con.cursor()
-        def list_product(self):
-            self.cur.execute("select list_price from XXIBM_PRODUCT_PRICING where item_number=1001")
-            result = self.cur.fetchall()
-            return result
-@app.route('/')
-    def product():
-        def db_query():
-            db = Database()
-            prod = db.list_product()
-            return prod
-        res = db_query()
-        return render_template('product.html', result=res, content_type='application/json')
+
+def index():
+            conn = MySQLdb.connect(host='mysql.gamification.svc.cluster.local',user='xxuser',passwd='welcome1')
+            cursor = conn.cursor()
+            cursor.execute("select list_price from XXIBM_PRODUCT_PRICING where item_number=1001")
+            data = cursor.fetchall()
+            return render_template('product.html', data=data)
 
 if __name__ == '__main__':
     application.run()
