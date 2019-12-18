c5c1684d4588ab581252c53562a66d3bb4f346d1
Nandita Saha <nasaha12@in.ibm.com>
2019-12-18 19:12:32 +0530

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -13,7 +13,7 @@ application = Flask(__name__, template_folder='templates')
 def index():
             conn = MySQLdb.connect(host='mysql.gamification.svc.cluster.local',user='xxuser',passwd='welcome1',db='sampledb')
             cursor = conn.cursor()
-            cursor.execute("select list_price, item_number from XXIBM_PRODUCT_PRICING LIMIT 10)
+            cursor.execute("select list_price, item_number from XXIBM_PRODUCT_PRICING LIMIT 10")
             data = cursor.fetchall()
             return render_template('product.html', data=data)
 
