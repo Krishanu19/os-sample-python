2820823b7a0c8ed39779028e5a3af0f613a581e3
Nandita Saha <nasaha12@in.ibm.com>
2019-12-18 17:42:27 +0530

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -1,26 +1,10 @@
-from flask import Flask, render_template
-#import mysql.connector
-import MySQLdb
+from flask import Flask
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-class Database:
-        def __init__(self):
-            host = "mysql.gamification.svc.cluster.local"
-            user = "xxuser"
-            password = "welcome1"
-            db = "XXIBM_PRODUCT_PRICING"
-            self.con = MySQLdb.connect(host='mysql.gamification.svc.cluster.local',user='xxuser',passwd='welcome1')
-            self.cur = self.con.cursor()
-        def list_price(self):
-            self.cur.execute("select list_price from XXIBM_PRODUCT_PRICING LIMIT 50")
-            result = self.cur.fetchall()
-            return result
-@application.route('/')
-def product():
-        def db_query():
-            db = Database()
-            listprice = db.list_price()
-            return listprice
-        res = db_query()
-        return render_template('product.html', result=res, content_type='application/json')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return "Hello Nuclear Geeks"
+
+if __name__ == '__main__':
+    app.run()
