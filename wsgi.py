a09452cf3ba777d52b79c05e5aedbe45c9546382
Nandita Saha <nasaha12@in.ibm.com>
2019-12-18 17:50:23 +0530

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello Nuclear Geeks"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    application.run()
+    app.run()
