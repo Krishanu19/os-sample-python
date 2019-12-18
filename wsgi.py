05f5dca2eebda5196079bfced9f415a6113ddf55
Nandita Saha <nasaha12@in.ibm.com>
2019-12-18 18:06:31 +0530

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-application = Flask(__name__,template_folder='../Templates')
+application = Flask(__name__,template_folder='/Krishanu19/os-sample-python/Templates/')
 
 @application.route('/', methods=['GET', 'POST'])
 def index():
