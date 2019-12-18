974f22240eaaf31bca2763e5cec4c58b12990604
Nandita Saha <nasaha12@in.ibm.com>
2019-12-18 19:34:53 +0530

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -12,7 +12,7 @@ application = Flask(__name__, template_folder='templates')
 
 def index():
             conn = MySQLdb.connect(host='mysql.gamification.svc.cluster.local',user='xxuser',passwd='welcome1',db='sampledb')
-            cursor = conn.cursor(buffered=True)
+            cursor = conn.cursor()
             cursor.execute("select list_price, item_number from XXIBM_PRODUCT_PRICING LIMIT 10")
             data = cursor.fetchall()
             print("Total number of rows in Laptop is: ", cursor.rowcount)
