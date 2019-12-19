95637bf72d7641924ddc9d44a60a9bdeacde6eeb
Nandita Saha <nasaha12@in.ibm.com>
2019-12-19 11:18:19 +0530

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -18,8 +18,8 @@ def index():
             print("Total number of rows in Laptop is: ", cursor.rowcount)
             print("\nPrinting record")
             for row in data:
-            print("list_price = ", row[0])
-            print("item_number = ", row[1])
+               print("list_price = ", row[0])
+               print("item_number = ", row[1])
             return render_template('product.html', data=data)
 
 if __name__ == '__main__':
