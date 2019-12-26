df6282002d092a2029efcdb3a64f725ded36e291
Krishanu Nayek <krnayek1@in.ibm.com>
2019-12-26 16:55:01 +0530

    by krishanu

--- wsgi.py
+++ wsgi.py
@@ -27,17 +27,17 @@ def data():
                print("catalogue_category = ", row[3])
              return render_template('product.html', data=data)
 
-#@application.route('/apparels', methods=['GET', 'POST'])
-#def apparels():
-#            conn = MySQLdb.connect(host='mysql.gamification.svc.cluster.local',user='xxuser',passwd='welcome1',db='sampledb')
-#            cursor = conn.cursor()
-#            if request.method == "POST":
-#              details = request.form
-#              query = "select distinct class_name from sampledb.XXIBM_PRODUCT_CATALOGUE WHERE FAMILY_NAME='Clothing'"
-#              cursor.execute(query)
-#              data = cursor.fetchall()
-#              print("Total number of rows: ", cursor.rowcount)
-#              return render_template('Apparels.html', data=data)
+@application.route('/apparels', methods=['GET', 'POST'])
+def apparels():
+            conn = MySQLdb.connect(host='mysql.gamification.svc.cluster.local',user='xxuser',passwd='welcome1',db='sampledb')
+            cursor = conn.cursor()
+            if request.method == "POST":
+              details = request.form
+              query = "select distinct class_name from sampledb.XXIBM_PRODUCT_CATALOGUE WHERE FAMILY_NAME='Clothing'"
+              cursor.execute(query)
+              data = cursor.fetchall()
+              print("Total number of rows: ", cursor.rowcount)
+              return render_template('Apparels.html', data=data)
 
 if __name__ == '__main__':
     application.run()
