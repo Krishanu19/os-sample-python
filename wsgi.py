d9539be331a09c8ef16ee74a2855dda37a1badc4
Krishanu Nayek <krnayek1@in.ibm.com>
2020-01-03 00:16:38 +0530

    by krishanu

--- wsgi.py
+++ wsgi.py
@@ -60,10 +60,19 @@ def athwear():
             cursor = conn.cursor()
             if request.method == "GET":
               details = request.form
-              query = "SELECT PS.DESCRIPTION FROM sampledb.XXIBM_PRODUCT_CATALOGUE PC , sampledb.XXIBM_PRODUCT_SKU PS , sampledb.XXIBM_PRODUCT_STYLE PST , sampledb.XXIBM_PRODUCT_PRICING PP WHERE PC.COMMODITY= PS.CATALOGUE_CATEGORY and PC.COMMODITY= PST.CATALOGUE_CATEGORY  AND PS.ITEM_NUMBER=PP.ITEM_NUMBER AND PC.CLASS_NAME='Athletic wear' and PP.IN_STOCK='Yes'"
+              query = "SELECT distinct PS.DESCRIPTION FROM sampledb.XXIBM_PRODUCT_CATALOGUE PC , sampledb.XXIBM_PRODUCT_SKU PS , sampledb.XXIBM_PRODUCT_STYLE PST , sampledb.XXIBM_PRODUCT_PRICING PP WHERE PC.COMMODITY= PS.CATALOGUE_CATEGORY and PC.COMMODITY= PST.CATALOGUE_CATEGORY  AND PS.ITEM_NUMBER=PP.ITEM_NUMBER AND PC.CLASS_NAME='Athletic wear' and PP.IN_STOCK='Yes'"
               cursor.execute(query)
-              clsData = cursor.fetchall()
-            return render_template('selection.html', athwear=clsData)              
+              desData = cursor.fetchall()
+              query = "SELECT distinct PST.BRAND FROM sampledb.XXIBM_PRODUCT_CATALOGUE PC , sampledb.XXIBM_PRODUCT_SKU PS , sampledb.XXIBM_PRODUCT_STYLE PST , sampledb.XXIBM_PRODUCT_PRICING PP WHERE PC.COMMODITY= PS.CATALOGUE_CATEGORY and PC.COMMODITY= PST.CATALOGUE_CATEGORY  AND PS.ITEM_NUMBER=PP.ITEM_NUMBER AND PC.CLASS_NAME='Athletic wear' AND PS.DESCRIPTION='Reflex Menâ€™s Track Jacket'and PP.IN_STOCK='Yes'" 
+              cursor.execute(query)
+              brndData = cursor.fetchall()
+              query = "SELECT distinct PS.SKU_ATTRIBUTE_VALUE1 FROM sampledb.XXIBM_PRODUCT_CATALOGUE PC , sampledb.XXIBM_PRODUCT_SKU PS , sampledb.XXIBM_PRODUCT_STYLE PST , sampledb.XXIBM_PRODUCT_PRICING PP WHERE PC.COMMODITY= PS.CATALOGUE_CATEGORY and PC.COMMODITY= PST.CATALOGUE_CATEGORY  AND PS.ITEM_NUMBER=PP.ITEM_NUMBER AND PC.CLASS_NAME='Athletic wear' AND PS.DESCRIPTION='Reflex Menâ€™s Track Jacket'and AND PST.BRAND='Reflex' and PP.IN_STOCK='Yes'" 
+              cursor.execute(query)
+              szData = cursor.fetchall()
+              query = "SELECT distinct PS.SKU_ATTRIBUTE_VALUE2 FROM sampledb.XXIBM_PRODUCT_CATALOGUE PC , sampledb.XXIBM_PRODUCT_SKU PS , sampledb.XXIBM_PRODUCT_STYLE PST , sampledb.XXIBM_PRODUCT_PRICING PP WHERE PC.COMMODITY= PS.CATALOGUE_CATEGORY and PC.COMMODITY= PST.CATALOGUE_CATEGORY  AND PS.ITEM_NUMBER=PP.ITEM_NUMBER AND PC.CLASS_NAME='Athletic wear' AND PS.DESCRIPTION='Reflex Menâ€™s Track Jacket'and AND PST.BRAND='Reflex' and PP.IN_STOCK='Yes'" 
+              cursor.execute(query)
+              clrData = cursor.fetchall()              
+            return render_template('selection.html', athwear=desData , athwear=brndData ,athwear=szData,athwear=clrData )              
 
 if __name__ == '__main__':
     application.run()
