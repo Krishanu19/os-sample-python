509528e1f8a8f31e3b1afa5e101c999a67661d2b
Nandita Saha <nasaha12@in.ibm.com>
2020-02-06 19:49:31 +0530

    remove br

--- wsgi.py
+++ wsgi.py
@@ -93,7 +93,7 @@ def athwear():
 def submit():
             conn = MySQLdb.connect(host='custom-mysql.gamification.svc.cluster.local',user='xxuser',passwd='welcome1',db='sampledb')
             cursor = conn.cursor()
-            selectbox1 = application.POST.get('selectbox1', '')
+            text = request.form['selectbox1']
             return 'You have chosen ' + str(selectbox1)
 
 if __name__ == '__main__':
